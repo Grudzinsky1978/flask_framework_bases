065625d46f45138327ac6e7b41f9e66897ecb7c6
Vladislav_Grudzinsky <vladik19780131@yandex.ru>
2024-08-27 00:31:33 +0300

    Flask. Bases. Lesson 2. Theme 9. abort

--- les_2/app_09.py
+++ les_2/app_09.py
@@ -2,7 +2,47 @@
 
 # ---
 # Функция abort
+# Может использоваться внутри view функций. Получив 404, прерывается работа текущей view и вызывается функция, которая декорирована errorhandler
 
 # if result is None:
 #     abort(404)
 
+
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import abort
+
+from db import get_blog
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+
+@app.route('/blog/<int:id>')
+def get_blog_by_id(id):
+    ...
+    # делаем запрос в БД для поиска статьи по id
+    result = get_blog(id)
+    if result is None:
+        abort(404)
+    ...
+    # возвращаем найденную в БД статью
+
+
+@app.errorhandler(404) # декоратор обрабатывает 404 ошибку
+def page_not_found(e):
+    app.logger.warning(e) # логирование в консоль
+    context = {
+        'title': 'Страница не найдена',
+        'url': request.base_url,
+    }
+    return render_template('404.html', **context), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
