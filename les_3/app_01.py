57295af30ac243f16ef5c8420365334705143ea9
Vladislav_Grudzinsky <vladik19780131@yandex.ru>
2024-08-30 20:49:00 +0300

    Flask. Bases. Lesson 3. Theme 1

--- les_3/app_01.py
+++ les_3/app_01.py
@@ -7,3 +7,19 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) # инициализация приложения Flask, создания объекта приложения
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db' # в файл конфигурации своего приложения добавляется констаната и её значение (sqlite - внутри одного файла)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@hostname/database_name'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://username:password@hostname/database_name'
+db = SQLAlchemy(app) # создан объект
+
+# создаётся каталог instance
+
+@app.route('/')
+def index():
+    return 'Hi!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
