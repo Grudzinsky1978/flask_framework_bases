4b6bb9206f80b94fe25cf2cd6df957b0135cc2de
Vladislav_Grudzinsky <vladik19780131@yandex.ru>
2024-08-30 19:45:27 +0300

    Flask. Bases. Lesson 3. Initial commit

---
+++ les_3/app_01.py
@@ -0,0 +1,9 @@
+# Flask SQLAlchemy. Для работы с базами данных
+# ОРМ - Объектно-реляционная модель, взаимодействие с базой данных не напрямую (с помощью запросов), а путём создания моделей данных
+
+# Установка pip install Flask-SQLAlchemy
+
+# Подключение к БД from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
