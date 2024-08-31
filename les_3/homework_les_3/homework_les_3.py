d589ccb0b83a058a381a36f0000616c6f57541c6
Vladislav_Grudzinsky <vladik19780131@yandex.ru>
2024-08-31 19:28:37 +0300

    Flask. Bases. Lesson 3. Homework. Complete

--- les_3/homework_les_3/homework_les_3.py
+++ les_3/homework_les_3/homework_les_3.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from model import db, User
 from flask_wtf.csrf import CSRFProtect
 
@@ -28,14 +28,17 @@ def init_db():
 
 
 @app.route('/reg/', methods=['GET', 'POST'])
-def register():
+def reg():
     form = RegForm()
     if request.method == 'POST' and form.validate():
         u_name = form.u_name.data
         u_surname = form.u_surname.data
         email = form.email.data
         passw = form.passw.data
-        print(u_surname, u_name, email, passw)
+        user = User(u_name=u_name, u_surname=u_surname, email=email, passw=passw)
+        db.session.add(user)
+        db.session.commit()
+        return f'{u_surname} {u_name}, вы успешно заргистрировались!'
     return render_template('reg.html', form=form)
 
 
--- les_3/homework_les_3/model.py
+++ les_3/homework_les_3/model.py
@@ -8,11 +8,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=False, nullable=False)
-    usersurname = db.Column(db.String(30), unique=False, nullable=False)
+    u_name = db.Column(db.String(30), unique=False, nullable=False)
+    u_surname = db.Column(db.String(30), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     passw = db.Column(db.String(120), unique=True, nullable=False)
     created_datetime = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'User({self.usersurname}, {self.username}, {self.email}, {self.passw})'
+        return f'User({self.u_name}, {self.u_surname}, {self.email}, {self.passw})'
